ccbee56f534f7012ae1159b78dd709b29694ef2e
luciano-mc <luciano.martiin@gmail.com>
2024-06-18 12:15:40 +0200

    fix styles

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -3,12 +3,12 @@ import './Aside.css';
 function Aside() {
   return (
     <div className="aside">
-      <svg width="250" height="156" viewBox="0 0 412 156" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg viewBox="0 0 412 156" fill="none" xmlns="http://www.w3.org/2000/svg">
         <rect x="0.5" y="0.5" width="411" height="155" rx="4.5" fill="#304C70" stroke="#DDDEDF"/>
         <rect x="22" y="36" width="327" height="10" fill="white"/>
         <rect x="22" y="62" width="195" height="10" fill="white"/>
       </svg>
-      <svg width="250" height="56" viewBox="0 0 408 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg viewBox="0 0 408 56" fill="none" xmlns="http://www.w3.org/2000/svg">
         <rect x="1" y="1" width="406" height="54" rx="3" stroke="#304C70" strokeWidth="2"/>
         <rect x="138" y="23" width="109" height="10" fill="#304C70"/>
         <path d="M255.812 22.0977C257.523 20.3867 259.586 19.5312 262 19.5312C264.414 19.5312 266.465 20.3867 268.152 22.0977C269.863 23.7852 270.719 25.8359 270.719 28.25C270.719 30.6641 269.863 32.7266 268.152 34.4375C266.465 36.125 264.414 36.9688 262 36.9688C259.586 36.9688 257.523 36.125 255.812 34.4375C254.125 32.7266 253.281 30.6641 253.281 28.25C253.281 25.8359 254.125 23.7852 255.812 22.0977Z" fill="#304C70"/>
